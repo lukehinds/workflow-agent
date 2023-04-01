6ca395858f8f41c2917ed018a95febaa0050b070
Luke Hinds <luke@stacklok.com>
2023-04-01 19:31:01 +0100

    Initial commit

---
+++ cmd/sign.go
@@ -0,0 +1,79 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/cobra"
+	"net/http"
+	"os"
+)
+
+const (
+	RequestTokenEnvKey = "ACTIONS_ID_TOKEN_REQUEST_TOKEN"
+	RequestURLEnvKey   = "ACTIONS_ID_TOKEN_REQUEST_URL"
+)
+
+// signCmd represents the sign command
+var signCmd = &cobra.Command{
+	Use:   "sign",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if os.Getenv(RequestTokenEnvKey) == "" {
+			fmt.Println("Request token not found in environment variable")
+			os.Exit(1)
+		}
+		if os.Getenv(RequestURLEnvKey) == "" {
+			fmt.Println("Request URL not found in environment variable")
+			os.Exit(1)
+		}
+
+		url := os.Getenv(RequestURLEnvKey) + "&audience=sigstore"
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", "Bearer "+os.Getenv(RequestTokenEnvKey))
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		var payload struct {
+			Value string `json:"value"`
+		}
+
+		decoder := json.NewDecoder(resp.Body)
+		if err := decoder.Decode(&payload); err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(payload.Value)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(signCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// signCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// signCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Copyright 2023 lukehinds
+*/
+package main
+
+import "github.com/lukehinds/workflow-agent/cmd"
+
+func main() {
+	cmd.Execute()
+}
